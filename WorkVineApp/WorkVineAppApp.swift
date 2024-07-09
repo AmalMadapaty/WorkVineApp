a3392510728aedbfbd516a9821dde4ad3debe8f4
AmalMadapaty <140457148+AmalMadapaty@users.noreply.github.com>
2024-07-09 10:49:43 -0500

    Initial Commit

---
+++ WorkVineApp/WorkVineAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkVineAppApp.swift
+//  WorkVineApp
+//
+//  Created by user264764 on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct WorkVineAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
